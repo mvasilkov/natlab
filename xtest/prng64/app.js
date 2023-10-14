10efac6fcf6f8d0894d60bae4191b23b020f2927
Mark Vasilkov <mvasilkov@gmail.com>
2023-10-14 23:24:05 +0300

    Thrust (natlib)

--- xtest/prng64/app.js
+++ xtest/prng64/app.js
@@ -6,11 +6,13 @@
 'use strict'
 
 import { createRequire } from 'node:module'
+import { Thrust } from 'natlib/prng/Thrust.js'
 import vectors from './vectors.js'
 
 const require = createRequire(import.meta.url)
 const nativeThrust = require('./build/Release/nativeThrust.node')
 
+// Thrust (native)
 for (const [start, vector] of Object.entries(vectors)) {
     nativeThrust.setState(BigInt(start))
     vector.forEach((a, n) => {
@@ -19,6 +21,20 @@ for (const [start, vector] of Object.entries(vectors)) {
 
         if (a !== b) {
             console.log(`!!! Thrust (native) start=${start} n=${n} a=${a} b=${b}`)
+            process.exit(1)
+        }
+    })
+}
+
+// Thrust (natlib)
+for (const [start, vector] of Object.entries(vectors)) {
+    const r = new Thrust(BigInt(start))
+    vector.forEach((a, n) => {
+        a = BigInt(a)
+        const b = r.randomUint64()
+
+        if (a !== b) {
+            console.log(`!!! Thrust (natlib) start=${start} n=${n} a=${a} b=${b}`)
             process.exit(1)
         }
     })
